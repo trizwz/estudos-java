c9259d69bac8e21f3ab4110e8b6036d3735f4753
Triz <beatrizalvesctt@gmail.com>
2024-08-25 02:00:23 -0300

    data types

--- maratona-java/src/academy/devdojo/maratonajava/introducao/aula02TiposPrimitivos.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/aula02TiposPrimitivos.java
@@ -2,8 +2,25 @@ package academy.devdojo.maratonajava.introducao;
 
 public class aula02TiposPrimitivos {
     public static void main(String[] args) {
+        //tipos primitivos aula 01 pt1 e pt2
         //int, double, float, char, byte, short, long, boolean
         int age = 18;
+        long numeroGrande = 1000000000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 127;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'f';
+
+        String nome = "KEN KANEKI";
+        var nome2 = "RIZE KAMISHINI";
+
+        System.out.println(nome);
         System.out.println("my age is: " + age);
+        System.out.println(falso);
+        System.out.println("char: "+caractere);
+        System.out.println(salarioDouble);
     }
 }
