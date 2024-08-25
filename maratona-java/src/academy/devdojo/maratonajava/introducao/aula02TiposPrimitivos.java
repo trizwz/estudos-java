6132b06abc60f67d93f7d9e5e3297c692f7ff2f2
Triz <beatrizalvesctt@gmail.com>
2024-08-24 21:49:32 -0300

    tipos primitivos aula01

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/aula02TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        int age = 18;
+        System.out.println("my age is: " + age);
+    }
+}
