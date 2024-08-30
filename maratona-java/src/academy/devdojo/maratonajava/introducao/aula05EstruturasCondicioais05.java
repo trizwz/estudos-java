7423a257c0d14f3f7be4fd2c3e3f842ec088d1bc
Triz <beatrizalvesctt@gmail.com>
2024-08-30 14:59:36 -0300

    switch 02

--- maratona-java/src/academy/devdojo/maratonajava/introducao/aula05EstruturasCondicioais05.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/aula05EstruturasCondicioais05.java
@@ -22,7 +22,39 @@ public class aula05EstruturasCondicioais05 {
             case 'D':
                 System.out.println("any.");
                 break;
-
         }
+
+
+        Scanner sc2 = new Scanner(System.in);
+
+        System.out.println("Tell me a day in a week");
+        byte day = sc2.nextByte();
+
+        switch (day) {
+            default:
+                System.out.println("Invalid option.");
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+        }
+
     }
 }
