0c5e3864431ef8b5496169632c8c0ac55d5f2678
Triz <beatrizalvesctt@gmail.com>
2024-09-05 01:56:18 -0300

    Repetition Structures

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/aula06EstruturasdeRepeticao01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class aula06EstruturasdeRepeticao01 {
+    public static void main(String[] args) {
+    int count = 0;
+    while(count < 10){
+        System.out.println(++count);
+    }
+
+    count = 0;
+    do{
+        System.out.println("numbers " + ++count);
+        } while (count < 10);
+    }
+}
