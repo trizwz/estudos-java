7c3cbc3f770e7e494f24fac446feb2b9fdeee249
Triz <beatrizalvesctt@gmail.com>
2024-09-05 02:10:02 -0300

    Repetition Structures >> for exercise

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/aula06EstruturasdeRepeticao02.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class aula06EstruturasdeRepeticao02 {
+    public static void main(String[] args) {
+        //imprima todos os numeros pares de 0 até 1000000
+
+
+        for(int i = 2; i <= 1000; i++){
+            if(i % 2 == 0)
+            System.out.println("numbers "+i);
+        }
+    }
+}
