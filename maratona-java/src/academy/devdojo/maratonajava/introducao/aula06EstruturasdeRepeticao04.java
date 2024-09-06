7b4660ac06a39acba30b79f68251d62872a257de
Triz <beatrizalvesctt@gmail.com>
2024-09-06 13:49:57 -0300

    Repetition Structures >> break lesson

---
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/aula06EstruturasdeRepeticao04.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class aula06EstruturasdeRepeticao04 {
+    public static void main(String[] args) {
+        //dado o valor do carro, descubra quantas vezes ele pode ser parcelado;
+        //condição valorParcela >= 1000;
+        double valorTotal = 30000;
+        for (int parcela = 1; parcela <= valorTotal; parcela++){
+            double valorParcela = valorTotal / parcela;
+            if(valorParcela >= 1000)  {
+                System.out.println("Parcela "+parcela+" R$ "+valorParcela);
+            }else{
+                break;
+            }
+
+        }
+    }
+}
+0
