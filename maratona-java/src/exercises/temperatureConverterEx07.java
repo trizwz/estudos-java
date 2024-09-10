53b9db0c2e42352a0bc58e675dcc685b91048493
Triz <beatrizalvesctt@gmail.com>
2024-09-10 16:04:31 -0300

    encerramento do scanner

--- maratona-java/src/exercises/temperatureConverterEx07.java
+++ maratona-java/src/exercises/temperatureConverterEx07.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class temperatureConverterEx07 {
     public static void main(String[] args) {
-        //conversor de temperatura
+        // conversor de temperatura
         System.out.println("Escolha um tipo de temperatura: ");
         System.out.println("1 - Fahrenheit\n2 - Réaumur\n3 - Rankine\n4 - Kelvin");
         Scanner typeTemperature = new Scanner(System.in);
@@ -13,27 +13,29 @@ public class temperatureConverterEx07 {
         System.out.println("Coloque o valor da temperatura: ");
         int c = typeTemperature.nextInt();
 
-       switch (typeTemp){
-           default:
-               System.out.println("Temperatura: "+c);
-               break;
-           case 1:
-               double farenheit = Math.round(c * 1.8 + 32);
-               System.out.println("Temperatura em Fahrenheit: "+ farenheit);
-               break;
-           case 2:
-               double reaumur = Math.round(c* 0.8);
-               System.out.println("Temperatura em Réaumur: "+reaumur);
-               break;
-           case 3:
-               double rankine =  Math.round((c + 273.15)* 9/5 );
-               System.out.println("Temperatura em Rankine: "+rankine);
-               break;
-           case 4:
-               double kelvin = Math.round(c  + 273.1);
-               System.out.println("Temperatura em Kelvin: "+kelvin);
-               break;
-       }
+        switch (typeTemp) {
+            default:
+                System.out.println("Temperatura: " + c);
+                break;
+            case 1:
+                double farenheit = Math.round(c * 1.8 + 32);
+                System.out.println("Temperatura em Fahrenheit: " + farenheit);
+                break;
+            case 2:
+                double reaumur = Math.round(c * 0.8);
+                System.out.println("Temperatura em Réaumur: " + reaumur);
+                break;
+            case 3:
+                double rankine = Math.round((c + 273.15) * 9 / 5);
+                System.out.println("Temperatura em Rankine: " + rankine);
+                break;
+            case 4:
+                double kelvin = Math.round(c + 273.1);
+                System.out.println("Temperatura em Kelvin: " + kelvin);
+                break;
+
+        }
+        typeTemperature.close();
 
     }
 }
