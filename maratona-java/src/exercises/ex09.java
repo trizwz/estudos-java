2d77561641a47e5b7c83405b2b0276cc769fb2e6
Triz <beatrizalvesctt@gmail.com>
2024-09-15 21:32:55 -0300

    cálculo >> média salarial

--- maratona-java/src/exercises/ex09.java
+++ maratona-java/src/exercises/ex09.java
@@ -1,4 +1,19 @@
 package exercises;
 
+import java.util.Scanner;
+
 public class ex09 {
+    public static void main(String[] args) {
+        /*Criar um programa que calcule a média de salários
+         de uma empresa, pedindo ao usuário a grade de funcionários
+         e os salários, e devolvendo a média salarial.
+         número de funcionarios : 4*/
+        Scanner sc = new Scanner(System.in);
+        double f1, f2, f3, f4, media;
+        f1 = sc.nextDouble();
+        f3 = sc.nextDouble();
+        f2 = sc.nextDouble();
+        f4 = sc.nextDouble();
+
+    }
 }
