8812fea020daa2d23b80936ae4f775a0b318a41e
Triz <beatrizalvesctt@gmail.com>
2024-09-21 01:57:43 -0300

    ex10 java

---
+++ maratona-java/src/exercises/ex10.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class ex10 {
+    public static void main(String[] args) {
+        /*altura da parede: ap, largura da parede: lp, ha: altura do azulejo, largura do azulejo: la*/
+    double  ap, lp, aa, la, qtd;
+    ap = 2.6;
+        lp = 1.6;
+        aa = 0.35;
+        la = 0.3;
+        qtd = (ap * lp) / (aa * la);
+    }
+}
